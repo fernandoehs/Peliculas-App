faa10b0ab4ecc939371bafd7637ad64084830a7a
Fernando <fernandoehs@gmail.com>
2021-02-10 00:43:21 -0500

    HomeScreen

---
+++ lib/widgets/movie_horizontal.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/models/pelicula_model.dart';
+
+class MovieHorizontal extends StatelessWidget {
+
+  final List<Pelicula> peliculas;
+  final Function siguientePagina;
+
+  MovieHorizontal({@required this.peliculas, this.siguientePagina});
+  
+  final _pageController = new PageController(
+    initialPage:1,
+    viewportFraction: 0.3,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final _screenSize =  MediaQuery.of(context).size;
+    
+    _pageController.addListener((){
+
+      if(_pageController.position.pixels >= _pageController.position.maxScrollExtent - 200){
+        siguientePagina();   
+      }
+    });
+
+
+    return Container(
+      height: _screenSize.height *0.2,
+      child: PageView.builder(
+        pageSnapping: false,
+        controller: _pageController,
+        //children:_tarjetas(context),
+        itemCount: peliculas.length,
+        itemBuilder: (context, i )=> _tarjeta(context, peliculas[i]),
+        
+      ),
+    );
+  }
+
+  Widget _tarjeta(BuildContext context, Pelicula pelicula){
+
+     final tarjeta = Container(
+      margin: EdgeInsets.only(right: 15.0),
+      child: Column(
+        children:<Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: FadeInImage(
+              image:NetworkImage(pelicula.getPosterImg()),
+              placeholder: AssetImage('assets/img/no-image.jpg'),
+              fit: BoxFit.cover,
+              height: 145.0,
+               ),
+          ),
+          
+           Text(
+           pelicula.title,
+           overflow: TextOverflow.ellipsis,
+             style:Theme.of(context).textTheme.caption,
+
+
+           )
+        ]
+      ),
+      );
+
+      return GestureDetector(
+        child: tarjeta,
+        onTap:(){
+          Navigator.pushNamed(context, 'detalle',arguments: pelicula);
+        } ,
+      );
+  }
+
+  // List<Widget> _tarjetas(BuildContext context){
+
+  //   return peliculas.map( (pelicula){
+  
+  //   }).toList();
+    
+  // }
+  
+}
